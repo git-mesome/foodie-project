c6367d4149d185f630650ab2d217b376fbc2b1ca
minseo-kim <kms12782@gmail.com>
2022-09-08 19:59:21 +0900

    Create Account Response Class

--- src/main/java/io/wisoft/foodie/project/web/dto/res/FindAccountResponse.java
+++ src/main/java/io/wisoft/foodie/project/web/dto/res/FindAccountResponse.java
@@ -8,7 +8,7 @@ public class FindAccountResponse {
     private String name;
     private String email;
 
-    public FindAccountResponse(String name, String email) {
+    public FindAccountResponse(String name) {
         this.name = name;
         this.email = email;
     }
