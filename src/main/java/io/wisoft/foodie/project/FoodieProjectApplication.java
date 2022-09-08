6d50a02ffdd4da3b5213bf4efa8642d7a4800ca2
minseo-kim <kms12782@gmail.com>
2022-09-08 19:58:23 +0900

    Insert Jpa Auditing

--- src/main/java/io/wisoft/foodie/project/FoodieProjectApplication.java
+++ src/main/java/io/wisoft/foodie/project/FoodieProjectApplication.java
@@ -2,7 +2,9 @@ package io.wisoft.foodie.project;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class FoodieProjectApplication {
 
