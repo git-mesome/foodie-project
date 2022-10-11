794756bacbecd580766df34ff4b6d4f94ff4a8b7
minseo-kim <kms12782@gmail.com>
2022-10-11 21:38:07 +0900

    Create Post Detail Response DTO

---
+++ src/main/java/io/wisoft/foodie/project/domain/post/web/dto/res/FindPostDetailResponse.java
@@ -0,0 +1,27 @@
+package io.wisoft.foodie.project.domain.post.web.dto.res;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import io.wisoft.foodie.project.domain.post.persistance.DealStatus;
+import io.wisoft.foodie.project.domain.post.persistance.Grade;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+public record FindPostDetailResponse(Long postId,
+                                     String authorNickname,
+                                     String authorProfilePath,
+                                     Grade authorGrade,
+                                     String title,
+                                     String content,
+                                     Integer hit,
+                                     Integer postLikes,
+                                     String siDo,
+                                     String siGunGu,
+                                     String eupMyeonDong,
+                                     DealStatus dealStatus,
+                                     @JsonFormat(shape = JsonFormat.Shape.STRING,
+                                             pattern = "yyyy-MM-dd HH:mm:ss", timezone = "Asia/Seoul") LocalDateTime createDate,
+                                     List<String> postImagePath) {
+
+
+}
