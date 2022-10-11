35ed8a3239b58dec985ed0d5e6a6809c6384a204
minseo-kim <kms12782@gmail.com>
2022-10-11 21:40:30 +0900

    Delete Class File

--- src/main/java/io/wisoft/foodie/project/global/web/validator/image/Custom.java
+++
@@ -1,4 +0,0 @@
-package io.wisoft.foodie.project.global.web.validator.image;
-
-public interface Custom {
-}
---
+++ src/test/java/io/wisoft/foodie/project/service/PostServiceTest.java
@@ -0,0 +1,34 @@
+package io.wisoft.foodie.project.service;
+
+import io.wisoft.foodie.project.domain.post.application.PostService;
+import io.wisoft.foodie.project.domain.post.persistance.Post;
+import io.wisoft.foodie.project.domain.post.persistance.PostRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+@SpringBootTest
+@Transactional
+class PostServiceTest {
+
+//    @Autowired
+//    PostRepository postRepository;
+//
+//    @Autowired
+//    PostService postService;
+//
+//    @Test
+//    public void save() throws Exception{
+//
+//        //given
+//        Post post = new Post();
+//
+//        postRepository.save(Post.builder()
+//                        .title("test")
+//                        .content("minseo")
+//                .build());
+//
+//    }
+
+}
