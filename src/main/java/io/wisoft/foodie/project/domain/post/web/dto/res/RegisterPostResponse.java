23aa323b9962d260d5ee3f414ca3cf0745e2ab62
minseo-kim <kms12782@gmail.com>
2022-10-11 21:42:00 +0900

    Update Post Register Response DTO

--- src/main/java/io/wisoft/foodie/project/domain/post/web/dto/res/RegisterPostResponse.java
+++ src/main/java/io/wisoft/foodie/project/domain/post/web/dto/res/RegisterPostResponse.java
@@ -1,29 +1,3 @@
 package io.wisoft.foodie.project.domain.post.web.dto.res;
-
-import com.fasterxml.jackson.annotation.JsonProperty;
-import io.wisoft.foodie.project.domain.post.Post;
-import io.wisoft.foodie.project.domain.post.persistance.PostEntity;
-import lombok.Getter;
-
-import java.util.List;
-
-@Getter
-public class RegisterPostResponse {
-
-    @JsonProperty("author_id")
-    private String authorId;
-    private String title;
-    private String content;
-
-    @JsonProperty("post_image_path")
-    private String postImagePath;
-
-
-    public RegisterPostResponse(final PostEntity post, final String postImagePath) {
-        this.authorId = post.getAuthor().getNickName();
-        this.title = post.getTitle();
-        this.content = post.getContent();
-        this.postImagePath = postImagePath;
-    }
-
+public record RegisterPostResponse(Long postId) {
 }
