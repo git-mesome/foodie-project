3d71c614529f9c14d9573e5754d4efa24708502f
minseo-kim <kms12782@gmail.com>
2022-10-12 21:48:03 +0900

    Update Grade Enum to Entity

--- src/main/java/io/wisoft/foodie/project/domain/post/persistance/Grade.java
+++ src/main/java/io/wisoft/foodie/project/domain/post/persistance/Grade.java
@@ -1,32 +1,7 @@
 package io.wisoft.foodie.project.domain.post.persistance;
 
-import io.wisoft.foodie.project.domain.account.persistance.AccountEntity;
-import lombok.Builder;
-import lombok.NoArgsConstructor;
+public enum Grade {
 
-import javax.persistence.*;
-import java.util.ArrayList;
-import java.util.List;
+    VIP
 
-@NoArgsConstructor
-@Entity
-public class Grade {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-
-    @Column(name = "name")
-    private String name;
-
-    @OneToMany(mappedBy = "grade")
-    private List<AccountEntity> accountEntities = new ArrayList<>();
-
-
-    @Builder
-    public Grade(Long id, String name, List<AccountEntity> accountEntities) {
-        this.id = id;
-        this.name = name;
-        this.accountEntities = accountEntities;
-    }
 }
